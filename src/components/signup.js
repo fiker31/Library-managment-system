c62c95fb2ed5976cc00839df49abe93e919ca04a
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-08-28 16:07:32 -0700

    removed addcircleoutlined icon

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Grid, Paper, Avatar, Typography, TextField, Button } from '@material-ui/core'
-import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
+//import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
+//import Radio from '@material-ui/core/Radio';
+//import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
@@ -16,9 +16,6 @@ const Signup = () => {
         <Grid>
             <Paper style={paperStyle}>
                 <Grid align='center'>
-                    <Avatar style={avatarStyle}>
-                        <AddCircleOutlineOutlinedIcon />
-                    </Avatar>
                     <h2 style={headerStyle}>Sign Up</h2>
                     <Typography variant='caption' gutterBottom>Please fill this form to create an account !</Typography>
                 </Grid>
@@ -27,10 +24,7 @@ const Signup = () => {
                     <TextField fullWidth label='Email' placeholder="Enter your email" />
                     <FormControl component="fieldset" style={marginTop}>
                         <FormLabel component="legend">Gender</FormLabel>
-                        <RadioGroup aria-label="gender" name="gender" style={{ display: 'initial' }}>
-                            <FormControlLabel value="female" control={<Radio />} label="Female" />
-                            <FormControlLabel value="male" control={<Radio />} label="Male" />
-                        </RadioGroup>
+                      
                     </FormControl>
                     <TextField fullWidth label='Phone Number' placeholder="Enter your phone number" />
                     <TextField fullWidth label='Password' placeholder="Enter your password"/>
