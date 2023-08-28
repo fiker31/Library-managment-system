da8c914dfff5ea65c0a2ab0f74ea97d43c655235
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-08-28 15:29:10 -0700

    edited app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import { useState } from "react";
-import BasicForm from "./components/BasicForm";
-import AdvancedForm from "./components/AdvancedForm";
-import "./App.css";
-
+import React from 'react';
+import './App.css';
+import SignInOutContainer from './containers';
 function App() {
-  const [view, setView] = useState("basic");
   return (
     <div className="App">
-      <nav>
-        <h3
-          onClick={() => setView("basic")}
-          style={{ color: view === "basic" ? "#fff" : "" }}
-        >
-          Basic
-        </h3>
-        <h3
-          onClick={() => setView("advanced")}
-          style={{ color: view === "advanced" ? "#fff" : "" }}
-        >
-          Advanced
-        </h3>
-      </nav>
-      {view === "basic" ? <BasicForm /> : <AdvancedForm />}
+     <SignInOutContainer/> 
     </div>
   );
-}
+} 
 
 export default App;
